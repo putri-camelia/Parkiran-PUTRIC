bf233a5d3256bb4c39c92b6040d35fdc99622b6e
irfansuhendra <irfansuhendra06@gmail.com>
2017-07-10 16:36:25 +0700

    commit ke 3

---
+++ admin/template/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profil Admin</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <script src="assets/js/bootstrap.min.js"></script>
+    <style type="text/css">
+      body {
+        background-image: url("assets/gambar/bg3.jpg");
+        background-size: 100%;
+        background-repeat: no-repeat;
+      }
+      table,h1,h4 {
+        color :#BFBFBF;
+      }
+      .active {
+        color :#22313F;
+      }
+      a {
+        color :#BFBFBF;
+      }
+      .btn {
+        color :#BFBFBF;
+      }
+      h3 {
+        color :#BFBFBF;
+      }
+    </style>
+  </head>
+  <body>
+      <div class="navbar transparent navbar-static-top">
+        <div class="container">
+          <div class="navbar-header">
+            <span class="navbar-brand"><img src="assets/gambar/parkir.png" width="80px" align="center"></span>
+          </div>
+          <ul class="nav navbar-nav navbar-right">
+          <li><a href="manajer.html"><img src="assets/gambar/home1.png" width="30px"></a></li>
+          </ul>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="nav navbar-nav">
+              <h1>PARKIRAN</h1>
+              <h4>Markir Boleh, Mangkir Jangan.</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>

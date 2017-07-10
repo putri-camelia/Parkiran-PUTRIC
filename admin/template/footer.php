2df48cf9b2030e4fba253d958bc6a1aff0c41f44
Theresia meiriati <theresiameiriati07@gmail.com>
2017-07-10 16:47:34 +0700

    menambahkan footer

---
+++ admin/template/footer.php
@@ -0,0 +1,17 @@
+        <div class="form-group">
+          <nav class="navbar teransparent navbar-fixed-bottom">
+            <div class="form-group" align="center">
+              <h5>Copyright Sistem Informasi Perparkiran</h5>
+            </div>
+          </nav>
+        </div>
+      </div>      
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script>
+   function simpan(){
+    alert('Data Tersimpan');
+  }
+  </script>
+  </body>
+</html>

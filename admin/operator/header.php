343980caac6190882aeb0eff50fd1f3f0e1333ed
sitihartini <hartini.elvyra@gmail.com>
2017-07-11 15:17:16 +0700

    menambahkan header operator

---
+++ admin/operator/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mobil Masuk</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+    <script src="assets/js/bootstrap.min.js"></script>
+    <style type="text/css">
+      body {
+        background-image: url("assets/gambar/bg3.jpg");
+        background-size: 100%;
+        background-repeat: no-repeat;
+      }
+      form {
+        border :2px; 
+        padding: 10%; 
+        border-radius: 5px; 
+        box-shadow: 0 20px 30px #101010;
+      }
+      h1,h2,h3,h4 {
+        color :#BFBFBF;
+      }
+      #id {
+        color :#BFBFBF;
+      }
+      .btn {
+        border-radius: 5px; 
+        border :2px;
+        color: #22313F; 
+        width: 100%; 
+      }
+      label {
+        color :white;
+      }
+      #id {
+        color :white;
+      }
+    </style>
+  </head>
+  <body>
+      <div class="navbar transparent navbar-static-top">
+        <div class="container">
+          <div class="navbar-header">
+            <span class="navbar-brand"><img src="assets/gambar/parkir.png" width="80px" align="center"></span>
+          </div>
+          <ul class="nav navbar-nav navbar-right">
+          <li><a href="index.html"><img src="assets/gambar/home1.png" width="30px"></a></li>
+          </ul>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="nav navbar-nav">
+              <h1>PARKIRAN</h1>
+              <h4>Markir Boleh, Mangkir Jangan.</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>

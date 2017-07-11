8bb8bc3bf1b69b3ddab6c5bdf724f93ceb8da310
Theresia meiriati <theresiameiriati07@gmail.com>
2017-07-11 15:14:01 +0700

    menambahkan footer operator

---
+++ admin/operator/footer.php
@@ -0,0 +1,10 @@
+<nav class="navbar teransparent navbar-fixed-bottom" id="footer">
+            <div class="form-group" align="center">
+              <h5>Copyright Sistem Informasi Perparkiran</h5>
+            </div>
+          </nav>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  </script>
+  </body>
+</html>

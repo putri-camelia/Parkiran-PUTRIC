6dca76706b9230d0981265e86dc125a0c1fbaf22
irfansuhendra <irfansuhendra06@gmail.com>
2017-07-12 00:17:49 +0700

    ubah tarif awal

---
+++ admin/view/UbahTarifUI.php
@@ -0,0 +1,17 @@
+<?php 
+require_once 'View.php'
+
+Class UbahTarifUI extends View
+{
+	public function getDataKeuangan(){
+		
+	}
+	public function ubahTarifParki(){
+
+	}
+	public function updateTarif(){
+
+	}
+}
+
+ ?>

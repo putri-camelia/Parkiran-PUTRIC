66d730859ff21d198878afe6acceb4e452c6c5c4
irfansuhendra <irfansuhendra06@gmail.com>
2017-07-11 15:25:01 +0700

    membuat folder template dan sidebar operator

---
+++ operator/template/sidebar.php
@@ -0,0 +1,23 @@
+ <div class="col-md-12">
+        <div class="section">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4" align="center">
+                <form action="login.html">
+                  <div class="form-group">
+                    <h2>Operator Mobil Masuk</h2>
+                  </div>
+                  <div class="form-group">
+                    <img src="assets/gambar/user.png" width="80px">
+                  </div>
+                  <div class="form-group" id="id">
+                    <dt>Nama</dt>
+                    <dd>Abcd</dd>
+                    <dt>Id Operator</dt>
+                    <dd>O211M</dd>
+                  </div>
+                  <div class="form-group">
+                   <button type="submit" class="btn btn-default">Keluar</button>
+                  </div>
+                </form>
+              </div>

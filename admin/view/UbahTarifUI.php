c7527bb80c32cb626f1bf259f4e6ca3a8f35aa2f
irfansuhendra <irfansuhendra06@gmail.com>
2017-07-13 16:48:11 +0700

    error dimana mana

--- admin/view/UbahTarifUI.php
+++ admin/view/UbahTarifUI.php
@@ -1,12 +1,12 @@
 <?php 
-require_once 'View.php'
+require_once 'View.php';
 
 class UbahTarifUI extends View
 {
 	public function ubahTarifParkir()
 	{
-		include_once 'model/KeuanganControl.php';
-		$kc = new KeuanganControl();
+		include_once 'model/Tarif.php';
+		$kc = new Tarif();
 		$data = $kc->UbahTarif();
 		include_once 'pages/formedittarif.php';
 		$this->end();
